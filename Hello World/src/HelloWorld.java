94ad9238b85eb669b4dbd83a53011295b3744a49
cgonzales3162 <cgonzales3162@001LAB153010823.neisd.ad>
2016-10-13 16:02:13 -0500

    commit 1

---
+++ Hello World/src/HelloWorld.java
@@ -0,0 +1,16 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        String name = "Carlos Gonzales";
+        int year = 1999;
+        int date = 11;
+        
+	System.out.print("Yo, I'm "+ name);
+	System.out.println("I was born in "+ year + " on september " + date);
+	System.out.print("the remainder of "+year+" and "+date+" is  ");
+	System.out.println(year%date);
+	}    
+
+}
